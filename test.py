e252468040f1f498634719cdc87c34409f253470
pedroigosl <pedroigosl@gmail.com>
2022-06-17 00:47:54 -0300

    refactor: remade edges and turned graph structure into adjacency set

--- test.py
+++ test.py
@@ -1,6 +1,13 @@
 from typing import List
 import logging
 import time
+import warnings
+
+warnings.simplefilter("always")
+
+# Import warning
+warning = f" Test import warning"
+warnings.warn(warning, ImportWarning)
 
 log_date = str(time.strftime("%d-%m-%y_%H:%M:%S"))
 log_name = f"logs/testlog_{log_date}.log"
@@ -21,3 +28,26 @@ test3 = (f"Graph properties changed. New properties are:\n"
 logging.info(test3)
 date = time.strftime("%d-%m-%y_%H:%M:%S")
 print(date)
+
+def time_flag():
+    return str(time.strftime("%d-%m-%y_%H:%M:%S"))
+
+def f(val):
+        it = None
+        if val == True:
+                warning = f" ({time_flag()}) RuntimeWarning - should show"
+                warnings.warn(warning, RuntimeWarning)
+                logging.warning(warning)
+                return it
+        return 5
+
+a = f(True)
+
+if not a:
+        print("ok")
+      
+group = set()
+
+a = True == False
+print(a)
+        
